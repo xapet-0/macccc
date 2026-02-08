3d6ed200f4fcc157c14cce7ee9dad6a7b2ac9c72
xapet <77368505+xapet-0@users.noreply.github.com>
2026-02-08 17:42:49 +0100

    Add Eagle OS README documentation

---
+++ eagle_os/config.py
@@ -0,0 +1,26 @@
+import os
+
+
+class BaseConfig:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "eagle-os-dev")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URI", "sqlite:///eagle_os.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BLACK_HOLE_INITIAL_DAYS = int(os.environ.get("BLACK_HOLE_INITIAL_DAYS", 100))
+    HEATMAP_THRESHOLDS = {
+        "active": int(os.environ.get("HEATMAP_ACTIVE_THRESHOLD", 3600)),
+        "burning": int(os.environ.get("HEATMAP_BURNING_THRESHOLD", 18000)),
+    }
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+
+
+CONFIG_MAP = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+}
